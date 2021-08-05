a3ebe26b03fa8e507f8bfc6fc3a4a3be93658a3b
SameerSK_Contus <sameer.g@contus.in>
2021-08-05 17:32:52 +0530

    Completed footer and hover elements code

--- WebAutomation/src/test/java/Runner/RunnerTest.java
+++ WebAutomation/src/test/java/Runner/RunnerTest.java
@@ -23,7 +23,7 @@ import net.masterthought.cucumber.sorting.SortingMethod;
 
 	@RunWith(Cucumber.class)
 	@CucumberOptions(features= "src/test/resources/Features/frontend",
-	glue={"steps"},
+	glue={"steps"}
 	
 	
 			)

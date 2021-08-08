fe33e219da8fd28acfd267408df45bbe16f8a1c3
SameerSK_Contus <sameer.g@contus.in>
2021-08-08 18:31:09 +0530

    cccc

--- WebAutomation/src/test/java/Runner/RunnerTest.java
+++ WebAutomation/src/test/java/Runner/RunnerTest.java
@@ -24,7 +24,8 @@ import net.masterthought.cucumber.sorting.SortingMethod;
 
 	@RunWith(Cucumber.class)
 	@CucumberOptions(features= "src/test/resources/Features/frontend",
-	glue={"steps"}
+	glue={"steps"},
+	plugin= {"timeline:test-output-thread/"}
 	
 	
 			)

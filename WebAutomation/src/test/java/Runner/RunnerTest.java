2c60a57b74da13a7d38e8d290902e09e18aee0ad
SameerSK_Contus <sameer.g@contus.in>
2021-08-13 14:05:42 +0530

    commit

--- WebAutomation/src/test/java/Runner/RunnerTest.java
+++ WebAutomation/src/test/java/Runner/RunnerTest.java
@@ -25,15 +25,57 @@ import net.masterthought.cucumber.sorting.SortingMethod;
 	@RunWith(Cucumber.class)
 	@CucumberOptions(features= "src/test/resources/Features",
 	glue={"steps"},
-	 tags="@test"
-	)
+	plugin = {"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:",
+			  "pretty","json:target/cucumber-reports/Cucumber.json", 
+			  "junit:target/cucumber-reports/Cucumber.xml",
+			  "html:target/cucumber-reports/htmlreports.html",
+			  "rerun:target/rerun.txt"},
+			   monochrome=true)
+	
 
 
 		
 	public class RunnerTest 
 	{	
 		
+		@BeforeClass
+		public static void logger() throws IOException {
 			
+			        
+			PropertyConfigurator.configure(System.getProperty("user.dir")+"/src/main/java/Resources/log4j.properties");
+
+		}
+
+		@AfterClass
+		public static void JVMreports() throws IOException {
+			File reportOutputDirectory = new File("target/JVM");
+			List<String> jsonFiles = new ArrayList<String>();
+			jsonFiles.add("target/cucumber-reports/Cucumber.json");
+			String buildNumber = "1";
+			String project = "gudsho";
+			Configuration configuration = new Configuration(reportOutputDirectory, project);
+			configuration.setBuildNumber(buildNumber);
+			configuration.addClassifications("Environment", "QA");
+			configuration.addClassifications("Browser", "Chrome");
+			configuration.addClassifications("Platform", "Windows 10");
+			configuration.setSortingMethod(SortingMethod.NATURAL);
+			ReportBuilder reportBuilder = new ReportBuilder(jsonFiles, configuration);
+			reportBuilder.generateReports();
+			Runtime r=Runtime.getRuntime();
+			r.addShutdownHook(new Thread(){
+				public void run() {
+					try {
+						AutomatedEmail.sendemail();
+					} catch (IOException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+				}
+			});
+			
+			
+			
+		}	
 			
 		
 	}
